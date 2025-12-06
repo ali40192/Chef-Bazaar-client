ac447954a559bf0feb293a68618fb6c6e75ad90f
Mohammad Ali <mohammadali40192@gmail.com>
2025-12-06 23:42:24 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Themes from "./Components/Themes/Themes";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-center font-bold">HW</h1>
+      <Themes></Themes>
+    </>
+  );
+}
+
+export default App;
